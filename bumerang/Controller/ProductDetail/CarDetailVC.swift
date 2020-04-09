288fff1748f701edd77e33d986fd74a3052bf10c
Guru <vhkshjd0514@gmail.com>
2020-04-10 05:25:09 +0900

    stress!!!!!!!!!

--- bumerang/Controller/ProductDetail/CarDetailVC.swift
+++ bumerang/Controller/ProductDetail/CarDetailVC.swift
@@ -154,7 +154,12 @@ class CarDetailVC: BaseViewController {
         let alert = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
         
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {(action) -> Void in
-            print("kkk")
+            
+               //TODO; firebase data write here
+               
+               
+               self.navigationController?.popViewController(animated: true)
+            
             
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
